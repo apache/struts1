2116d3249cfea29a6a8f56f32ee9b11c4814c0de
Craig R. McClanahan <craigmcc@apache.org>
2000-09-07 01:35:36 +0000

    Add the initial tags for the new "struts-logic.tld" library.  These tags include:
    - Conditional execution based on comparison of a specified variable
      to a specified constant value [equal, greaterEqual, greaterThan,
      lessEqual, lessThan, notEqual].
    
    - Conditional execution based on substring matching (match, notMatch).
    
    - Conditional execution based on presence or absence of a particular
      item (bean, cookie, header, parameter, or bean property) [present,
      notPresent]
    
    - Control flow and looping [forward, iterate, redirect]
    
    This library, and the related "beans and properties" library, has an
    associated test suite in the struts-test.war web application, so you can
    validate that they individually work for you.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/struts/trunk@47776 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/share/org/apache/struts/taglib/logic/ConditionalTagBase.java
@@ -0,0 +1,223 @@
+/*
+ * $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/taglib/logic/ConditionalTagBase.java,v 1.1 2000/09/07 01:35:35 craigmcc Exp $
+ * $Revision: 1.1 $
+ * $Date: 2000/09/07 01:35:35 $
+ *
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+
+package org.apache.struts.taglib.logic;
+
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+import org.apache.struts.util.MessageResources;
+
+
+/**
+ * Abstract base class for the various conditional evaluation tags.
+ *
+ * @author Craig R. McClanahan
+ * @version $Revision: 1.1 $ $Date: 2000/09/07 01:35:35 $
+ */
+
+public abstract class ConditionalTagBase extends TagSupport {
+
+
+    // ------------------------------------------------------------- Properties
+
+
+    /**
+     * The name of the cookie to be used as a variable.
+     */
+    protected String cookie = null;
+
+    public String getCookie() {
+        return (this.cookie);
+    }
+
+    public void setCookie(String cookie) {
+        this.cookie = cookie;
+    }
+
+
+    /**
+     * The name of the HTTP request header to be used as a variable.
+     */
+    protected String header = null;
+
+    public String getHeader() {
+        return (this.header);
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+
+    /**
+     * The message resources for this package.
+     */
+    protected static MessageResources messages =
+        MessageResources.getMessageResources
+        ("org.apache.struts.taglib.logic.LocalStrings");
+
+
+    /**
+     * The name of the JSP bean to be used as a variable (if
+     * <code>property</code> is not specified), or whose property is to be
+     * accessed (if <code>property</code> is specified).
+     */
+    protected String name = null;
+
+    public String getName() {
+        return (this.name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    /**
+     * The name of the HTTP request parameter to be used as a variable.
+     */
+    protected String parameter = null;
+
+    public String getParameter() {
+        return (this.parameter);
+    }
+
+    public void setParameter(String parameter) {
+        this.parameter = parameter;
+    }
+
+
+    /**
+     * The name of the bean property to be used as a variable.
+     */
+    protected String property = null;
+
+    public String getProperty() {
+        return (this.property);
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+
+    // --------------------------------------------------------- Public Methods
+
+
+    /**
+     * Perform the test required for this particular tag, and either evaluate
+     * or skip the body of this tag.
+     *
+     * @exception JspException if a JSP exception occurs
+     */
+    public int doStartTag() throws JspException {
+
+        if (condition())
+            return (EVAL_BODY_INCLUDE);
+        else
+            return (SKIP_BODY);
+
+    }
+
+
+    /**
+     * Evaluate the remainder of the current page normally.
+     *
+     * @exception JspException if a JSP exception occurs
+     */
+    public int doEndTag() throws JspException {
+
+        return (EVAL_PAGE);
+
+    }
+
+
+    /**
+     * Reset custom attributes to their default state.
+     */
+    public void releaseCustomAttributes() {
+
+        cookie = null;
+        header = null;
+        name = null;
+        parameter = null;
+        property = null;
+
+    }
+
+
+    // ------------------------------------------------------ Protected Methods
+
+
+    /**
+     * Evaluate the condition that is being tested by this particular tag,
+     * and return <code>true</code> if the nested body content of this tag
+     * should be evaluated, or <code>false</code> if it should be skipped.
+     * This method must be implemented by concrete subclasses.
+     *
+     * @exception JspException if a JSP exception occurs
+     */
+    protected abstract boolean condition() throws JspException;
+
+
+}
