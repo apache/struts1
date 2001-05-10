ac4e0a3ec278382e2c16747b05797cf1695d361d
Craig R. McClanahan <craigmcc@apache.org>
2001-05-10 03:13:33 +0000

    Add standard actions that let you integrate other web application components via RequestDispatcher.forward() and RequestDispatcher.include(), but still take advantage of the preprocessing performed by the controller servlet (such as auto-population and validation of ActionForm beans).
    git-svn-id: https://svn.apache.org/repos/asf/struts/trunk@48335 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/share/org/apache/struts/actions/ForwardAction.java
@@ -0,0 +1,170 @@
+/*
+ * $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/actions/ForwardAction.java,v 1.1 2001/05/10 03:13:32 craigmcc Exp $
+ * $Revision: 1.1 $
+ * $Date: 2001/05/10 03:13:32 $
+ *
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999-2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Struts", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+
+package org.apache.struts.actions;
+
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.util.MessageResources;
+
+
+/**
+ * <p>An <strong>Action</strong> that forwards to the context-relative
+ * URI specified by the <code>parameter</code> property of our associated
+ * <code>ActionMapping</code>.  This can be used to integrate Struts with
+ * other business logic components that are implemented as servlets (or JSP
+ * pages), but still take advantage of the Struts controller servlet's
+ * functionality (such as processing of form beans).</p>
+ *
+ * <p>To configure the use of this Action in your
+ * <code>struts-config.xml</code> file, create an entry like this:</p>
+ *
+ * <code>
+ *   &lt;action path="/saveSubscription"
+ *           type="org.apache.struts.actions.ForwardAction"
+ *           name="subscriptionForm"
+ *          scope="request"
+ *          input="/subscription.jsp"
+ *      parameter="/path/to/processing/servlet"&gt;
+ * </code>
+ *
+ * <p>which will forward control to the context-relative URI specified by the
+ * <code>parameter</code> attribute.</p>
+ *
+ * @author Craig R. McClanahan
+ * @version $Revision: 1.1 $ $Date: 2001/05/10 03:13:32 $
+ */
+
+public class ForwardAction extends Action {
+
+
+    // ----------------------------------------------------- Instance Variables
+
+
+    /**
+     * The message resources for this package.
+     */
+    protected static MessageResources messages =
+     MessageResources.getMessageResources
+        ("org.apache.struts.actions.LocalStrings");
+
+
+    // --------------------------------------------------------- Public Methods
+
+
+    /**
+     * Process the specified HTTP request, and create the corresponding HTTP
+     * response (or forward to another web component that will create it).
+     * Return an <code>ActionForward</code> instance describing where and how
+     * control should be forwarded, or <code>null</code> if the response has
+     * already been completed.
+     *
+     * @param mapping The ActionMapping used to select this instance
+     * @param actionForm The optional ActionForm bean for this request (if any)
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet exception occurs
+     */
+    public ActionForward perform(ActionMapping mapping,
+				 ActionForm form,
+				 HttpServletRequest request,
+				 HttpServletResponse response)
+	throws IOException, ServletException {
+
+        // Create a RequestDispatcher the corresponding resource
+        String path = mapping.getParameter();
+        if (path == null) {
+            response.sendError
+                (HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                 messages.getMessage("forward.path"));
+            return (null);
+        }
+        RequestDispatcher rd =
+            servlet.getServletContext().getRequestDispatcher(path);
+        if (rd == null) {
+            response.sendError
+                (HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                 messages.getMessage("forward.rd", path));
+            return (null);
+        }
+
+        // Forward control to the specified resource
+        rd.forward(request, response);
+
+        // Tell the controller servlet that the response has been created
+        return (null);
+
+    }
+
+
+}
+
