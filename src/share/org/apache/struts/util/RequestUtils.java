e9aea0b666eeb53b7df6f63b551b7de464cd3892
Craig R. McClanahan <craigmcc@apache.org>
2001-06-01 20:21:40 +0000

    Generate "&amp;" instead of "&" in request URIs so that the output is still legal XML syntax in the value returned by computeURL().
    PR:  Bugzilla #1938
    Submitted by :  Alessandro Vernet <avernet@scdi.org> (also supplied
    the patch - thanks!)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/struts/branches/STRUTS_1_0_BRANCH@48383 13f79535-47bb-0310-9956-ffa450edef68

--- src/share/org/apache/struts/util/RequestUtils.java
+++ src/share/org/apache/struts/util/RequestUtils.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/util/RequestUtils.java,v 1.14.2.1 2001/06/01 18:45:03 craigmcc Exp $
- * $Revision: 1.14.2.1 $
- * $Date: 2001/06/01 18:45:03 $
+ * $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/util/RequestUtils.java,v 1.14.2.2 2001/06/01 20:21:40 craigmcc Exp $
+ * $Revision: 1.14.2.2 $
+ * $Date: 2001/06/01 20:21:40 $
  *
  * ====================================================================
  *
@@ -95,7 +95,7 @@ import org.apache.struts.upload.MultipartRequestHandler;
  * in the Struts controller framework.
  *
  * @author Craig R. McClanahan
- * @version $Revision: 1.14.2.1 $ $Date: 2001/06/01 18:45:03 $
+ * @version $Revision: 1.14.2.2 $ $Date: 2001/06/01 20:21:40 $
  */
 
 public class RequestUtils {
@@ -364,7 +364,7 @@ public class RequestUtils {
                         url.append('?');
                         question = true;
                     } else
-                        url.append('&');
+                        url.append("&amp;");
                     url.append(URLEncoder.encode(key));
                     url.append('='); // Interpret null as "no value"
                 } else if (value instanceof String) {
@@ -372,7 +372,7 @@ public class RequestUtils {
                         url.append('?');
                         question = true;
                     } else
-                        url.append('&');
+                        url.append("&amp;");
                     url.append(URLEncoder.encode(key));
                     url.append('=');
                     url.append(URLEncoder.encode((String) value));
@@ -383,7 +383,7 @@ public class RequestUtils {
                             url.append('?');
                             question = true;
                         } else
-                            url.append('&');
+                            url.append("&amp;");
                         url.append(URLEncoder.encode(key));
                         url.append('=');
                         url.append(URLEncoder.encode(values[i]));
