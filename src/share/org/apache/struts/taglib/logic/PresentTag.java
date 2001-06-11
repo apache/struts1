425c0a34c2594899a871655b1c58b0211326b747
Craig R. McClanahan <craigmcc@apache.org>
2001-06-11 17:34:40 +0000

    Update processing of the <logic:present> and <logic:notPresent> tags to reflect the fact that the four-argument version of RequestUtils.lookup() will now throw an exception if the bean named by the "name" parameter is not found.  It is my belief that all other tags that call this lookup() method are correctly dealing with the exception.
    PR: Bugzilla #2112
    Submitted by:	Johan Compagner <jcompagner@j-com.nl>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/struts/branches/STRUTS_1_0_BRANCH@48408 13f79535-47bb-0310-9956-ffa450edef68

--- src/share/org/apache/struts/taglib/logic/PresentTag.java
+++ src/share/org/apache/struts/taglib/logic/PresentTag.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/taglib/logic/PresentTag.java,v 1.7 2001/02/12 21:49:58 craigmcc Exp $
- * $Revision: 1.7 $
- * $Date: 2001/02/12 21:49:58 $
+ * $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/taglib/logic/PresentTag.java,v 1.7.2.1 2001/06/11 17:34:40 craigmcc Exp $
+ * $Revision: 1.7.2.1 $
+ * $Date: 2001/06/11 17:34:40 $
  *
  * ====================================================================
  *
@@ -77,7 +77,7 @@ import org.apache.struts.util.RequestUtils;
  * is present for this request.
  *
  * @author Craig R. McClanahan
- * @version $Revision: 1.7 $ $Date: 2001/02/12 21:49:58 $
+ * @version $Revision: 1.7.2.1 $ $Date: 2001/06/11 17:34:40 $
  */
 
 public class PresentTag extends ConditionalTagBase {
@@ -133,8 +133,13 @@ public class PresentTag extends ConditionalTagBase {
                 getHeader(header);
             present = (value != null);
         } else if (name != null) {
-            Object value =
-                RequestUtils.lookup(pageContext, name, property, scope);
+            Object value = null;
+            try {
+                value =
+                    RequestUtils.lookup(pageContext, name, property, scope);
+            } catch (JspException e) {
+                value = null;
+            }
             present = (value != null);
         } else if (parameter != null) {
             String value =
