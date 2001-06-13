87222540724385d20e7c96bf195468831bc5e9bf
Craig R. McClanahan <craigmcc@apache.org>
2001-06-13 03:49:46 +0000

    Port the fix for bugzilla #2133.
    git-svn-id: https://svn.apache.org/repos/asf/struts/trunk@48421 13f79535-47bb-0310-9956-ffa450edef68

--- src/share/org/apache/struts/taglib/html/BaseHandlerTag.java
+++ src/share/org/apache/struts/taglib/html/BaseHandlerTag.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/taglib/html/BaseHandlerTag.java,v 1.5 2001/06/10 03:53:31 craigmcc Exp $
- * $Revision: 1.5 $
- * $Date: 2001/06/10 03:53:31 $
+ * $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/taglib/html/BaseHandlerTag.java,v 1.6 2001/06/13 03:49:46 craigmcc Exp $
+ * $Revision: 1.6 $
+ * $Date: 2001/06/13 03:49:46 $
  *
  * ====================================================================
  *
@@ -71,7 +71,7 @@ import org.apache.struts.util.MessageResources;
  * appropriate implementations of these.
  *
  * @author Don Clasen
- * @version $Revision: 1.5 $ $Date: 2001/06/10 03:53:31 $
+ * @version $Revision: 1.6 $ $Date: 2001/06/13 03:49:46 $
  */
 
 public abstract class BaseHandlerTag extends BodyTagSupport {
@@ -161,6 +161,12 @@ public abstract class BaseHandlerTag extends BodyTagSupport {
 
     /** Identifier associated with component.  */
     private String styleId = null;
+
+// Other Common Attributes
+
+    /** The advisory title of this element. */
+    private String title = null;
+
 
     // ------------------------------------------------------------- Properties
 
@@ -387,6 +393,19 @@ public abstract class BaseHandlerTag extends BodyTagSupport {
         return styleId;
     }
 
+// Other Common Elements
+
+    /** Returns the advisory title attribute. */
+    public String getTitle() {
+        return title;
+    }
+
+    /** Sets the advisory title attribute. */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
     // --------------------------------------------------------- Public Methods
 
 
@@ -417,6 +436,7 @@ public abstract class BaseHandlerTag extends BodyTagSupport {
 	style = null;
 	styleClass = null;
         styleId = null;
+        title = null;
 
     }
 
@@ -443,6 +463,11 @@ public abstract class BaseHandlerTag extends BodyTagSupport {
         if (styleId != null) {
             styles.append(" id=\"");
             styles.append(styleId);
+            styles.append("\"");
+        }
+        if (title != null) {
+            styles.append(" title=\"");
+            styles.append(title);
             styles.append("\"");
         }
         return styles.toString();
