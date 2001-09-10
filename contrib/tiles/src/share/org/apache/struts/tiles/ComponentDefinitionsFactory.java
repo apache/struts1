18fa388a81c385e8369ad5c4886a63a895d747fd
Cedric Dumoulin <cedric@apache.org>
2001-09-10 13:01:30 +0000

    Update documentation
    git-svn-id: https://svn.apache.org/repos/asf/struts/trunk@48551 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/tiles/src/share/org/apache/struts/tiles/ComponentDefinitionsFactory.java
+++ contrib/tiles/src/share/org/apache/struts/tiles/ComponentDefinitionsFactory.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/cvs/jakarta-struts/contrib/tiles/src/share/org/apache/struts/tiles/Attic/ComponentDefinitionsFactory.java,v 1.1 2001/08/01 14:36:42 cedric Exp $
- * $Revision: 1.1 $
- * $Date: 2001/08/01 14:36:42 $
+ * $Header: /home/cvs/jakarta-struts/contrib/tiles/src/share/org/apache/struts/tiles/Attic/ComponentDefinitionsFactory.java,v 1.2 2001/09/10 13:01:30 cedric Exp $
+ * $Revision: 1.2 $
+ * $Date: 2001/09/10 13:01:30 $
  * $Author: cedric $
  *
  */
@@ -16,36 +16,34 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletContext;
 
 /**
-* Component repository interface.
- * This interface allows to retrieve an definition by its name, independently of the 
+ * Component repository interface.
+ * This interface allows to retrieve an definition by its name, independently of the
  * factory implementation.
  * Implementation must be Serializable, in order to be compliant with web Container
  * having this constraint (Weblogic 6.x).
 */
-public interface ComponentDefinitionsFactory extends Serializable 
+public interface ComponentDefinitionsFactory extends Serializable
 {
-   
+
    /**
      * Get a definition by its name.
-     * @throws DefinitionsFactoryException An error occur while getting 
-definition.
+     * @param name Name of requested definition.
+     * @param request Current servelet request
+     * @param servletContext current servlet context
+     * @throws DefinitionsFactoryException An error occur while getting definition.
      * @throws NoSuchDefinitionException No definition found for specified name
-     * Implementation can throw more accurate exception as a subclass of this 
-exception
-   @roseuid 3AF6F7E203DB
+     * Implementation can throw more accurate exception as a subclass of this exception
    */
    public ComponentDefinition getDefinition(String name, ServletRequest request, ServletContext servletContext) throws NoSuchDefinitionException,DefinitionsFactoryException;
-   
+
    /**
      * Init factory.
      * This method is called exactly once immediately after factory creation in
      * case of internal creation (by DefinitionUtil).
      * @param servletContext Servlet Context passed to newly created factory.
-     * @param properties Map of name/property passed to newly created factory. 
-Map can contains
-     * more properties than requested.
+     * @param properties Map of name/property passed to newly created factory.
+     * Map can contains more properties than requested.
      * @throws DefinitionsFactoryException An error occur during initialization.
-   @roseuid 3AF6F7E30080
    */
    public void initFactory(ServletContext servletContext, Map properties) throws DefinitionsFactoryException;
 }
