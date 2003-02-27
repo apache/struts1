40b47495635b5cb904ded25975ccc1a9b2c5d066
James Mitchell <jmitchell@apache.org>
2003-02-27 01:59:17 +0000

    Initial checkin.  Many more will follow.
    git-svn-id: https://svn.apache.org/repos/asf/struts/trunk@50302 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/struts/taglib/html/TestButtonTag.java
@@ -0,0 +1,253 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999-2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Struts", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.struts.taglib.html;
+
+import java.util.Locale;
+
+import javax.servlet.jsp.PageContext;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.apache.cactus.JspTestCase;
+import org.apache.struts.Globals;
+
+/**
+ * Suite of unit tests for the
+ * <code>org.apache.struts.taglib.bean.ButtonTag</code> class.
+ *
+ * @author James Mitchell
+ */
+public class TestButtonTag extends JspTestCase {
+
+    /**
+     * Defines the testcase name for JUnit.
+     *
+     * @param theName the testcase's name.
+     */
+    public TestButtonTag(String theName) {
+        super(theName);
+    }
+
+    /**
+     * Start the tests.
+     *
+     * @param theArgs the arguments. Not used
+     */
+    public static void main(String[] theArgs) {
+        junit.awtui.TestRunner.main(new String[] {TestButtonTag.class.getName()});
+    }
+
+    /**
+     * @return a test suite (<code>TestSuite</code>) that includes all methods
+     *         starting with "test"
+     */
+    public static Test suite() {
+        // All methods starting with "test" will be executed in the test suite.
+        return new TestSuite(TestButtonTag.class);
+    }
+
+    private void runMyTest(String whichTest, String locale){
+    	pageContext.setAttribute(Globals.LOCALE_KEY, new Locale(locale, locale), PageContext.SESSION_SCOPE);
+		request.setAttribute("runTest", whichTest);
+        try {
+			pageContext.forward("/test/org/apache/struts/taglib/html/TestButtonTag.jsp");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail("There is a problem that is preventing the tests to continue!");
+		}
+    }
+    
+	private void formatAndTest(String compare, String output) {
+		//fix for introduced carriage return / line feeds
+		output = output.replaceAll("\r","");
+		output = output.replaceAll("\n","");
+		output = output.trim();
+	    assertEquals(compare, output);
+	}
+
+    /*
+     * Testing ButtonTag.
+     */
+    public void testButtonProperty(){ 
+    	runMyTest("testButtonProperty", "");
+	}
+    public void testButtonPropertyAccesskey(){ 
+    	runMyTest("testButtonPropertyAccesskey", "");
+	}
+    public void testButtonPropertyAlt(){ 
+    	runMyTest("testButtonPropertyAlt", "");
+	}
+    public void testButtonPropertyAltKey1(){ 
+    	runMyTest("testButtonPropertyAltKey1", "");
+	}
+    public void testButtonPropertyAltKey2(){ 
+    	runMyTest("testButtonPropertyAltKey2", "");
+	}
+    public void testButtonPropertyAltKey_fr1(){ 
+    	runMyTest("testButtonPropertyAltKey1_fr", "fr");
+	}
+    public void testButtonPropertyAltKey_fr2(){ 
+    	runMyTest("testButtonPropertyAltKey2_fr", "fr");
+	}
+    public void testButtonPropertyDisabled_True(){ 
+    	runMyTest("testButtonPropertyDisabled_True", "");
+	}
+    public void testButtonPropertyDisabled_False1(){ 
+    	runMyTest("testButtonPropertyDisabled_False1", "");
+	}
+    public void testButtonPropertyDisabled_False2(){ 
+    	runMyTest("testButtonPropertyDisabled_False2", "");
+	}
+    public void testButtonPropertyOnblur(){ 
+    	runMyTest("testButtonPropertyOnblur", "");
+	}
+
+    public void testButtonPropertyOnchange(){ 
+    	runMyTest("testButtonPropertyOnchange", "");
+	}
+
+    public void testButtonPropertyOnclick(){ 
+    	runMyTest("testButtonPropertyOnclick", "");
+	}
+
+    public void testButtonPropertyOndblclick(){ 
+    	runMyTest("testButtonPropertyOndblclick", "");
+	}
+
+    public void testButtonPropertyOnfocus(){ 
+    	runMyTest("testButtonPropertyOnfocus", "");
+	}
+
+    public void testButtonPropertyOnkeydown(){ 
+    	runMyTest("testButtonPropertyOnkeydown", "");
+	}
+
+    public void testButtonPropertyOnkeypress(){ 
+    	runMyTest("testButtonPropertyOnkeypress", "");
+	}
+
+    public void testButtonPropertyOnkeyup(){ 
+    	runMyTest("testButtonPropertyOnkeyup", "");
+	}
+
+    public void testButtonPropertyOnmousedown(){ 
+    	runMyTest("testButtonPropertyOnmousedown", "");
+	}
+
+    public void testButtonPropertyOnmousemove(){ 
+    	runMyTest("testButtonPropertyOnmousemove", "");
+	}
+
+    public void testButtonPropertyOnmouseout(){ 
+    	runMyTest("testButtonPropertyOnmouseout", "");
+	}
+
+    public void testButtonPropertyOnmouseover(){ 
+    	runMyTest("testButtonPropertyOnmouseover", "");
+	}
+
+    public void testButtonPropertyOnmouseup(){ 
+    	runMyTest("testButtonPropertyOnmouseup", "");
+	}
+
+    public void testButtonPropertyStyle(){ 
+    	runMyTest("testButtonPropertyStyle", "");
+	}
+
+    public void testButtonPropertyStyleClass(){ 
+    	runMyTest("testButtonPropertyStyleClass", "");
+	}
+
+    public void testButtonPropertyStyleId(){ 
+    	runMyTest("testButtonPropertyStyleId", "");
+	}
+
+    public void testButtonPropertyTabindex(){ 
+    	runMyTest("testButtonPropertyTabindex", "");
+	}
+
+    public void testButtonPropertyTitle(){ 
+    	runMyTest("testButtonPropertyTitle", "");
+	}
+
+    public void testButtonPropertyTitleKey(){ 
+    	runMyTest("testButtonPropertyTitleKey", "");
+	}
+
+    public void testButtonPropertyTitleKey_fr(){ 
+    	runMyTest("testButtonPropertyTitleKey_fr", "fr");
+	}
+
+    public void testButtonPropertyValue(){ 
+    	runMyTest("testButtonPropertyValue", "");
+	}
+
+    public void testButtonPropertyBodyContent(){ 
+    	runMyTest("testButtonPropertyBodyContent", "");
+	}
+
+    public void testButtonPropertyBodyContentMessageKey(){ 
+    	runMyTest("testButtonPropertyBodyContentMessageKey", "");
+	}
+
+    public void testButtonPropertyBodyContentMessageKey_fr(){ 
+    	runMyTest("testButtonPropertyBodyContentMessageKey_fr", "fr");
+	}
+
+
+
+
+}
