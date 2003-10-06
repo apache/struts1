d1f58756bc717dd0cdf9329df5e7eab82d4390fd
Robert Leland <rleland@apache.org>
2003-10-06 04:04:56 +0000

    Fix License, No functional Changes
    git-svn-id: https://svn.apache.org/repos/asf/struts/trunk@51135 13f79535-47bb-0310-9956-ffa450edef68

--- src/share/org/apache/struts/validator/DynaValidatorActionForm.java
+++ src/share/org/apache/struts/validator/DynaValidatorActionForm.java
@@ -1,13 +1,13 @@
 /*
- * $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/validator/DynaValidatorActionForm.java,v 1.10 2003/07/23 00:47:05 dgraham Exp $
- * $Revision: 1.10 $
- * $Date: 2003/07/23 00:47:05 $
+ * $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/validator/DynaValidatorActionForm.java,v 1.11 2003/10/06 04:04:56 rleland Exp $
+ * $Revision: 1.11 $
+ * $Date: 2003/10/06 04:04:56 $
  *
  * ====================================================================
  *
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 1999-2003 The Apache Software Foundation.  All rights
+ * Copyright (c) 2000-2003 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -23,7 +23,7 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:
+ *    any, must include the following acknowledgement:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowlegement may appear in the software itself,
@@ -35,8 +35,8 @@
  *    permission, please contact apache@apache.org.
  *
  * 5. Products derived from this software may not be called "Apache"
- *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -56,6 +56,7 @@
  * individuals on behalf of the Apache Software Foundation.  For more
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
+ *
  */
 
 package org.apache.struts.validator;
@@ -84,7 +85,7 @@ import org.apache.struts.action.ActionMapping;
  * for validation rules.</li></ul>
  *
  * @author David Winterfeldt
- * @version $Revision: 1.10 $ $Date: 2003/07/23 00:47:05 $
+ * @version $Revision: 1.11 $ $Date: 2003/10/06 04:04:56 $
  * @since Struts 1.1
  */
 public class DynaValidatorActionForm extends DynaValidatorForm implements DynaBean, Serializable {
