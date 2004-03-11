8fe5e879b27b2d06c7fb2f2af8630805d690a6df
Ted Nathan Husted <husted@apache.org>
2004-03-11 03:26:02 +0000

    Remove unused members; update license.
    git-svn-id: https://svn.apache.org/repos/asf/struts/trunk@51493 13f79535-47bb-0310-9956-ffa450edef68

--- src/example/org/apache/struts/webapp/example/memory/MemoryDatabasePlugIn.java
+++ src/example/org/apache/struts/webapp/example/memory/MemoryDatabasePlugIn.java
@@ -1,65 +1,22 @@
 /*
- * $Header: /home/cvs/jakarta-struts/src/example/org/apache/struts/webapp/example/memory/MemoryDatabasePlugIn.java,v 1.11 2004/01/13 12:48:44 husted Exp $
- * $Revision: 1.11 $
- * $Date: 2004/01/13 12:48:44 $
+ * $Header: /home/cvs/jakarta-struts/src/example/org/apache/struts/webapp/example/memory/MemoryDatabasePlugIn.java,v 1.12 2004/03/11 03:26:02 husted Exp $
+ * $Revision: 1.12 $
+ * $Date: 2004/03/11 03:26:02 $
  *
- * ====================================================================
+ * Copyright 2000-2004 Apache Software Foundation
  *
- * The Apache Software License, Version 1.1
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Copyright (c) 1999-2002 The Apache Software Foundation.  All rights
- * reserved.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowledgement:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
- *
- * 4. The names "The Jakarta Project", "Struts", and "Apache Software
- *    Foundation" must not be used to endorse or promote products derived
- *    from this software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache"
- *    nor may "Apache" appear in their name, without prior written
- *    permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-
 package org.apache.struts.webapp.example.memory;
 
 
@@ -68,14 +25,12 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
 import javax.servlet.ServletException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.ActionServlet;
 import org.apache.struts.action.PlugIn;
 import org.apache.struts.config.ModuleConfig;
-import org.apache.struts.util.LabelValueBean;
 import org.apache.struts.webapp.example.Constants;
 
 /**
@@ -92,19 +47,13 @@ import org.apache.struts.webapp.example.Constants;
  * <strong>NOT</strong> assume that files written here will survive a restart
  * of your servlet container.</p>
  *
- * @version $Revision: 1.11 $ $Date: 2004/01/13 12:48:44 $
+ * @version $Revision: 1.12 $ $Date: 2004/03/11 03:26:02 $
  */
 
 public final class MemoryDatabasePlugIn implements PlugIn {
 
 
     // ----------------------------------------------------- Instance Variables
-
-
-    /**
-     * The application configuration for our owning module.
-     */
-    private ModuleConfig config = null;
 
 
     /**
@@ -166,8 +115,6 @@ public final class MemoryDatabasePlugIn implements PlugIn {
         database = null;
         servlet = null;
         database = null;
-        config = null;
-
     }
 
 
@@ -186,7 +133,6 @@ public final class MemoryDatabasePlugIn implements PlugIn {
                  pathname + "'");
 
         // Remember our associated configuration and servlet
-        this.config = config;
         this.servlet = servlet;
 
         // Construct a new database and make it available
