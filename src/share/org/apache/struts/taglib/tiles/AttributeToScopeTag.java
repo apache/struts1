f197ff9663bc444ef9926c35c4d1b02c32f0aa61
Steve Raeburn <sraeburn@apache.org>
2004-03-14 06:23:54 +0000

    Convert to Apache License 2.0
    git-svn-id: https://svn.apache.org/repos/asf/struts/trunk@51502 13f79535-47bb-0310-9956-ffa450edef68

--- src/share/org/apache/struts/taglib/tiles/AttributeToScopeTag.java
+++ src/share/org/apache/struts/taglib/tiles/AttributeToScopeTag.java
@@ -1,7 +1,26 @@
 /*
-  *  $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/taglib/tiles/AttributeToScopeTag.java,v 1.6 2003/09/28 17:02:22 dgraham Exp $
-  *  $Revision: 1.6 $
-  *  $Date: 2003/09/28 17:02:22 $
+ * \$Header\$
+ * \$Revision\$
+ * \$Date\$
+ *
+ * Copyright 2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+  *  $Header: /home/cvs/jakarta-struts/src/share/org/apache/struts/taglib/tiles/AttributeToScopeTag.java,v 1.7 2004/03/14 06:23:49 sraeburn Exp $
+  *  $Revision: 1.7 $
+  *  $Date: 2004/03/14 06:23:49 $
   *
   *  ====================================================================
   *
