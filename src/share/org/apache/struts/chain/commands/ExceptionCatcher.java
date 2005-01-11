1e690f20fbfb750eb237f26b0a20ac78f039cd8e
James Mitchell <jmitchell@apache.org>
2005-01-11 15:23:54 +0000

    remove duplicate import
    git-svn-id: https://svn.apache.org/repos/asf/struts/core/trunk@124908 13f79535-47bb-0310-9956-ffa450edef68

--- src/share/org/apache/struts/chain/commands/ExceptionCatcher.java
+++ src/share/org/apache/struts/chain/commands/ExceptionCatcher.java
@@ -25,7 +25,6 @@ import org.apache.commons.chain.Filter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.struts.chain.Constants;
-import org.apache.struts.chain.Constants;
 
 
 /**
