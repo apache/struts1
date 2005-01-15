43a7ac101c19d60ff246d0999578c9e9e21328bc
James Mitchell <jmitchell@apache.org>
2005-01-15 23:55:02 +0000

    putting tests back, i'm moving too fast ;)
    git-svn-id: https://svn.apache.org/repos/asf/struts/core/trunk@125320 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/struts/action/TestActionMessage.java
@@ -0,0 +1,85 @@
+/*
+ * $Id$ 
+ *
+ * Copyright 2002-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.struts.action;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit tests for the <code>org.apache.struts.action.ActionMessage</code> class.
+ *
+ * @version $Rev$ $Date$
+ */
+public class TestActionMessage extends TestCase {
+    
+    protected ActionMessage amWithNoValue = null;
+    
+    protected ActionMessage amWithOneValue = null;
+    
+    /**
+     * Defines the testcase name for JUnit.
+     *
+     * @param theName the testcase's name.
+     */
+    public TestActionMessage(String theName) {
+        super(theName);
+    }
+
+    /**
+     * Start the tests.
+     *
+     * @param theArgs the arguments. Not used
+     */
+    public static void main(String[] theArgs) {
+        junit.awtui.TestRunner.main(
+            new String[] { TestActionMessage.class.getName()});
+    }
+
+    /**
+     * @return a test suite (<code>TestSuite</code>) that includes all methods
+     *         starting with "test"
+     */
+    public static Test suite() {
+        // All methods starting with "test" will be executed in the test suite.
+        return new TestSuite(TestActionMessage.class);
+    }
+
+    public void setUp() {
+        amWithNoValue = new ActionMessage("amWithNoValue");
+        amWithOneValue =
+            new ActionMessage("amWithOneValue", new String("stringValue"));
+    }
+
+    public void tearDown() {
+        amWithNoValue = null;
+    }
+    
+    public void testActionMessageWithNoValue() {
+        assertTrue(amWithNoValue.getValues() == null);
+        assertTrue(amWithNoValue.getKey() == "amWithNoValue");
+    }
+
+    public void testActionMessageWithAStringValue() {
+        Object[] values = amWithOneValue.getValues();
+        assertTrue(values != null);
+        assertTrue(values[0].equals("stringValue"));
+        assertTrue(amWithOneValue.getKey() == "amWithOneValue");
+    }
+}
---
+++ src/test/org/apache/struts/mock/MockActionServlet.java
@@ -0,0 +1,101 @@
+/*
+ * $Id$ 
+ *
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.apache.struts.mock;
+
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletConfig;
+
+
+import org.apache.struts.action.ActionServlet;
+
+/**
+ * <p>Mock <strong>ActionServlet</strong> object for low-level unit tests
+ * of Struts controller components.  Coarser grained tests should be
+ * implemented in terms of the Cactus framework, instead of the mock
+ * object classes.</p>
+ *
+ * <p><strong>WARNING</strong> - Only getter methods for servletContext and
+ * servletConfig are provided, plus additional methods to configure this
+ * object as necessary.  Methods for unsupported operations will throw
+ * <code>UnsupportedOperationException</code>.</p>
+ *
+ * <p><strong>WARNING</strong> - Because unit tests operate in a single
+ * threaded environment, no synchronization is performed.</p>
+ *
+ * @version $Rev$ $Date$
+ */
+public class MockActionServlet extends ActionServlet
+{
+  protected ServletContext servletContext;
+  protected ServletConfig servletConfig;
+
+    /**
+     * Constructor.
+     */
+  public MockActionServlet( ServletContext servletContext, ServletConfig servletConfig  )
+  {
+  this.servletContext = servletContext;
+  this.servletConfig = servletConfig;
+  }
+
+    /**
+     * Constructor.
+     */
+  public MockActionServlet( )
+  {
+  }
+
+    /**
+     * Set property
+     * @param servletContext
+     */
+  public void setServletContext( ServletContext servletContext )
+  {
+  this.servletContext = servletContext;
+  }
+
+    /**
+     * Get property
+     * @return
+     */
+  public ServletContext getServletContext(  )
+  {
+  return servletContext;
+  }
+
+    /**
+     * Set property
+     * @param servletConfig
+     */
+  public void setServletConfig( ServletConfig servletConfig )
+  {
+  this.servletConfig = servletConfig;
+  }
+
+    /**
+     * Get property
+     * @return
+     */
+  public ServletConfig getServletConfig(  )
+  {
+  return servletConfig;
+  }
+}
