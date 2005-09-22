57f9dcfea310171a9dcbf7a558f146c122632a06
James Mitchell <jmitchell@apache.org>
2005-09-22 15:15:10 +0000

    Updates to the nightly shell script to better handle dated logs Update 'clean-lib' target to depend on 'clean'
    git-svn-id: https://svn.apache.org/repos/asf/struts/core/branches/STRUTS_1_2_BRANCH@290959 13f79535-47bb-0310-9956-ffa450edef68

--- nightly.sh
+++ nightly.sh
@@ -3,9 +3,16 @@
 
 cd /home/jmitchell/svn/STRUTS_1_2_BRANCH
 
-ant clean > nightly.log; svn status >> nightly.log; ant download-dependencies nightly >> nightly.log & tail -f nightly.log
+export TODAY=`date +%Y%m%d`
+export LOGFILE=/home/jmitchell/svn/STRUTS_1_2_BRANCH/nightly-$TODAY.log
+ant clean-lib > $LOGFILE
+svn status >> $LOGFILE 
+ant download-dependencies nightly >> $LOGFILE
 
-scp /home/jmitchell/svn/STRUTS_1_2_BRANCH/release/upload/*bin* cvs.apache.org:/www/cvs.apache.org/builds/jakarta-struts/maven/STRUTS_1_2_BRANCH/nightly/binary >> nightly.log
-scp /home/jmitchell/svn/STRUTS_1_2_BRANCH/release/upload/*lib* cvs.apache.org:/www/cvs.apache.org/builds/jakarta-struts/maven/STRUTS_1_2_BRANCH/nightly/lib >> nightly.log
-scp /home/jmitchell/svn/STRUTS_1_2_BRANCH/release/upload/*src* cvs.apache.org:/www/cvs.apache.org/builds/jakarta-struts/maven/STRUTS_1_2_BRANCH/nightly/src >> nightly.log
-scp nightly.log /www/cvs.apache.org/builds/jakarta-struts/maven/STRUTS_1_2_BRANCH/nightly/logs
+scp /home/jmitchell/svn/STRUTS_1_2_BRANCH/release/upload/*bin* cvs.apache.org:/www/cvs.apache.org/builds/jakarta-struts/maven/STRUTS_1_2_BRANCH/nightly/binary >> $LOGFILE
+scp /home/jmitchell/svn/STRUTS_1_2_BRANCH/release/upload/*lib* cvs.apache.org:/www/cvs.apache.org/builds/jakarta-struts/maven/STRUTS_1_2_BRANCH/nightly/lib >> $LOGFILE
+scp /home/jmitchell/svn/STRUTS_1_2_BRANCH/release/upload/*src* cvs.apache.org:/www/cvs.apache.org/builds/jakarta-struts/maven/STRUTS_1_2_BRANCH/nightly/src >> $LOGFILE
+scp $LOGFILE cvs.apache.org:/www/cvs.apache.org/builds/jakarta-struts/maven/STRUTS_1_2_BRANCH/nightly/logs
+
+
+
