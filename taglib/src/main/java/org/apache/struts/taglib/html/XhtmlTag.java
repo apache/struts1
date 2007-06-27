d16e5f8ca95893855aad1733338519eb4416aa4d
Paul C. Benedict Jr <pbenedict@apache.org>
2007-06-27 06:01:14 +0000

    STR-1175: Fix javadoc
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts1/trunk@551064 13f79535-47bb-0310-9956-ffa450edef68

--- taglib/src/main/java/org/apache/struts/taglib/html/XhtmlTag.java
+++ taglib/src/main/java/org/apache/struts/taglib/html/XhtmlTag.java
@@ -30,8 +30,8 @@ import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.TagSupport;
 
 /**
- * This tag tells all other html taglib tags to render themselves in xhtml. It
- * has no attributes; it's presence in a page turns on xhtml. <p>
+ * This tag tells all other html taglib tags to render themselves in xhtml. 
+ * Its presence in a page turns on xhtml. <p>
  * Example:<br/> &lt;html:xhtml/&gt; </p>
  */
 public class XhtmlTag extends TagSupport {
